250f7532cd5c19b610b7930afe1fb8da335b6a37
Mark Urquhart <markurquhart@Marks-MBP.hsd1.vt.comcast.net>
2022-07-21 07:12:08 -0400

    Added pagination and bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/app.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import ReactPaginate from 'react-paginate'
 import { BASE_URL } from './globals'
 import CharacterList from './components/CharacterList'
 import CharacterDetails from './components/CharacterDetails'
@@ -25,10 +26,30 @@ const App = () => {
     getCharacters()
   }, [])
 
+  const handlePageClick = (data) => {
+    console.log(data.selected)
+  }
+
   return (
 
     <div>
       <h1 className="title">Marvel Characters</h1>
+      <ReactPaginate 
+      previousLabel={'Previous'}
+      nextLabel={'Next'}
+      pageCount={17}
+      onPageChange={handlePageClick}
+      containerClassName={'pagination justify-content-center'}
+      pageClassName={'page-item'}
+      pageLinkClassName={'page-link'}
+      previousClassName={'page-item'}
+      previousLinkClassName={'page-link'}
+      nextClassName={'page-item'}
+      nextLinkClassName={'page-link'}
+      breakClassName={'page-item'}
+      breakLinkClassName={'page-link'}    
+      activeClassName={'active'}          
+      />
       {selectedCharacter ? (
         <CharacterDetails selectedCharacter={selectedCharacter} goBack={goBack}/>
       ) : (
